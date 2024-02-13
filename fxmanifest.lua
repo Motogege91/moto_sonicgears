6d21acf0e85a45be122d2482605581bd2b702f95
Motogege91 <stefanosgambatibusiness@gmail.com>
2024-02-13 11:59:14 +0100

    commit iniziale

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'MOTOGEGE91'
+description 'motogege91 - Ls Customs.'
+version '1.0.0'
+
+
+this_is_a_map 'yes'
+
+data_file('DLC_ITYP_REQUEST')('stream/archetypes/moto_LS_anim.ytyp')
+
+
+
+
+
+
+
