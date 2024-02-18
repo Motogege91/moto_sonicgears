2fc27a4f405298dd790e6b6f46a458edf85e49d7
Motogege91 <62526004+Motogege91@users.noreply.github.com>
2024-02-18 10:41:22 +0100

    Update fxmanifest.lua
    Added detailss for FX manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,13 +2,12 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'MOTOGEGE91'
-description 'motogege91 - Ls Customs.'
-version '1.0.0'
+description 'Motogege Atelier - Sonic Gears music shop mlo'
+version '1.0.2'
 
 
 this_is_a_map 'yes'
 
-data_file('DLC_ITYP_REQUEST')('stream/archetypes/moto_LS_anim.ytyp')
 
 
 
